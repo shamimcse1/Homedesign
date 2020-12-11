89f8667cb5a7ab79fb003ceb86b0d2c1b98d3afb
mdsha <mdshamimh529@gmail.com>
2020-12-12 01:04:07 +0600

    Frist time

---
+++ app/src/main/java/com/codercamp/homedesigns/EnglishBook/EnglishBookActivity.java
@@ -0,0 +1,15 @@
+package com.codercamp.homedesigns.EnglishBook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codercamp.homedesigns.R;
+
+public class EnglishBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_english_book);
+    }
+}
